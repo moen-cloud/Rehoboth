e33f02aca5e6aba861488e8d54ae67a73267009b
moen-cloud <moenmwangi@gmail.com>
2026-07-02 12:52:31 +0300

    Fix logo size and increase navbar height to accommodate logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
       }}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-26">
+        <div className="flex justify-between items-center h-22">
 
           {/* Logo */}
           <Link to="/" className="flex items-center" onClick={closeMobileMenu}>
